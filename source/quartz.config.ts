1ae102d91aa4d92ea593b5d51bd5ccbe21020418
aCadogan <atimetowait@gmail.com>
2025-07-18 19:02:13 -0400

    fix color

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -40,7 +40,7 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#23236dff",
+          light: "#7e7e7eff",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
